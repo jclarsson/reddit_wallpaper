2b2f2c95a65d51299e3105f952b9022524034835
James Larsson <jclarsson@users.noreply.github.com>
2021-10-28 11:51:39 -0400

    Update reddit_wallpaper.py

--- reddit_wallpaper.py
+++ reddit_wallpaper.py
@@ -2,7 +2,7 @@
 
 import gi, os, json, threading, time, requests, subprocess, sys
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gio, GLib
+from gi.repository import Gtk, GdkPixbuf, Gio, GLib
 
 if "requests" not in sys.modules:
     print("Error: missing dependency \"requests\". Please run \"pip3 install requests\" to fix this. Some systems may include it in their package manager as python-requests.")
@@ -76,8 +76,17 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
         box.pack_start(self.stack2fb, True, True, 0)
         self.stack2.add(box)
 
+        self.stack3 = Gtk.ScrolledWindow()
+        self.stack3fb = Gtk.FlowBox()
+        self.stack3fb.set_valign(Gtk.Align.START)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        box.pack_start(Gtk.Label(label="    Select an icon to set your wallpaper", xalign=0), False, False, 10)
+        box.pack_start(self.stack3fb, True, True, 0)
+        self.stack3.add(box)
+
         self.left.add_named(self.stack1, "a")
         self.left.add_named(self.stack2, "b")
+        self.left.add_named(self.stack3, "c")
         self.left.set_visible_child_name("a")
 
         # Setup right side. This is the settings panel, and it can hide or show.
@@ -120,26 +129,66 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
         self.refreshbutton.add(image)
         self.refreshbutton.set_sensitive(False)
         self.refreshbutton.connect("clicked", self.refresh)
+        self.refreshbutton.set_tooltip_text("Refresh wallpapers from Reddit")
         hb.pack_start(self.refreshbutton)
 
-        # button = Gtk.Button()
-        # icon = Gio.ThemedIcon(name="open-menu-symbolic")
-        # image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
-        # button.add(image)
-        # hb.pack_end(button)
+        self.hbsort = Gtk.ComboBoxText()
+
+        for index, i in enumerate(self.settings.sort_types):
+            self.hbsort.append_text(i)
+            if i == self.settings.get("sort"):
+                self.hbsort.set_active(index)
+        self.hbsort.connect("changed", self.update_sort_from_hb)
+        self.hbsort.set_sensitive(False)
+        hb.pack_start(self.hbsort)
 
         button = Gtk.ToggleButton()
         icon = Gio.ThemedIcon(name="preferences-system-symbolic")
         image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
         button.add(image)
         button.connect("clicked", self.on_settings_click)
-        button.set_active(True)
+        if self.settings.get("show_settings_pane") is True:
+            button.set_active(True)
+        button.set_tooltip_text("Configuration")
+        hb.pack_end(button)
+
+        self.recents = Gtk.Popover()
+        self.recentsbox = Gtk.ScrolledWindow()
+        self.recentsfb = Gtk.FlowBox()
+        self.recentsfb.set_valign(Gtk.Align.START)
+        box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        opendirbtn = Gtk.Button.new_with_label("Open wallpaper directory")
+        opendirbtn.connect("clicked", self.show_dir)
+        box.pack_start(self.recentsfb, True, True, 0)
+        box.pack_start(opendirbtn, False, False, 10)
+        self.recentsbox.add(box)
+        self.recents.add(self.recentsbox)
+        self.recentsbox.set_min_content_height(400)
+        self.recentsbox.set_policy(hscrollbar_policy=Gtk.PolicyType.NEVER, vscrollbar_policy=Gtk.PolicyType.AUTOMATIC)
+        self.recentsbox.show_all()
+        self.recents.set_position(Gtk.PositionType.BOTTOM)
+
+        button = Gtk.MenuButton(popover=self.recents)
+        icon = Gio.ThemedIcon(name="document-open-recent-symbolic")
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        button.add(image)
+        button.set_tooltip_text("Restore previously-downloaded wallpapers")
         hb.pack_end(button)
 
         self.show_all()
+        if self.settings.get("show_settings_pane") is True:
+            self.hbsort.hide()
+        else:
+            self.right.hide()
 
         t1 = threading.Thread(target=self.load)
         t1.start()
+
+        t2 = threading.Thread(target=self.refreshRecents)
+        t2.start()
+
+    def show_dir(self, sender):
+        subprocess.run(['xdg-open', self.storage_directory + "/Wallpapers/"])
 
     def save_settings(self, sender):
         self.apply_settings(sender, False)
@@ -153,17 +202,50 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
         self.left.show_all()
         self.left.set_visible_child_name("b")
         self.refreshbutton.set_sensitive(True)
+        self.hbsort.set_sensitive(True)
         self.srbutton.set_sensitive(True)
 
-    def refresh(self, sender):
+    def refreshRecents(self):
+        print("Refreshing recents")
+        for i in self.recentsfb.get_children():
+            self.recentsfb.remove(i)
+        files = sorted([os.path.join(self.storage_directory + "/Wallpapers/",i) for i in os.listdir(self.storage_directory + "/Wallpapers/")], key=os.path.getmtime)[::-1]
+        for entry in files:
+            if (entry.endswith('.png') or entry.endswith('.jpg') or entry.endswith('.jpeg') or entry.endswith('.tif') or entry.endswith('.tiff') or entry.endswith('.gif') or entry.endswith('.webp') or entry.endswith('.bmp')):
+                button = Gtk.Button()
+                try:
+                    pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_size(entry,200,200)
 
+                    image = Gtk.Image().new_from_pixbuf(pixbuf)
+
+                    button.add(image)
+                except gi.repository.GLib.Error:
+                    button.set_label(entry.replace(self.storage_directory + "/Wallpapers/", ""))
+
+                button.connect("clicked", self.set_wallpaper_from_recent, entry.replace(self.storage_directory + "/Wallpapers/", ""))
+                button.set_tooltip_text(entry.replace(self.storage_directory + "/Wallpapers/", ""))
+
+                self.recentsfb.add(button)
+        self.recentsfb.show_all()
+
+    def set_wallpaper_from_recent(self, sender, title):
+        self.run_wallpaper_manager(title) 
+
+    def refresh(self, sender):
         self.refreshbutton.set_sensitive(False)
+        self.hbsort.set_sensitive(False)
         self.srbutton.set_sensitive(False)
         self.left.set_visible_child_name("a")
         for i in self.stack2fb.get_children():
             self.stack2fb.remove(i)
         t1 = threading.Thread(target=self.load)
         t1.start()
+
+    def update_sort_from_hb(self, sender):
+
+        if self.settings.set("sort", self.hbsort.get_active_text()) is not False:
+            self.refresh(sender)
+
 
     def load(self):
         GLib.idle_add(self.set_progress, 0.05, "Fetching listing from Reddit")
@@ -173,7 +255,7 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
                 max_listings = "&"
             else:
                 max_listings = "/?"
-            self.reddit = json.loads(requests.get("https://api.reddit.com/r/" + self.settings.get("subreddits") + "/" + self.settings.get("sort") + max_listings + "limit=50", headers={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'}).content)
+            self.reddit = json.loads(requests.get("https://api.reddit.com/r/" + self.settings.get("subreddits") + "/" + self.settings.get("sort") + max_listings + "limit=100", headers={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'}).content)
 
         except:
             GLib.idle_add(self.set_progress, 0, "Failed to load Reddit. Please check your internet connection and try again.")
@@ -183,12 +265,22 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
 
         images = []
 
-        for index, x in enumerate(self.reddit["data"]["children"]):
+        try:
+            for index, x in enumerate(self.reddit["data"]["children"]):
 
-            image = x["data"]["preview"]["images"][0]["source"]  # this contains metadata about the image
+                try:
+                    image = x["data"]["preview"]["images"][0]["source"]  # this contains metadata about the image
 
-            if int(image["width"]) >= int(self.settings.get("min_width")) and int(image["height"]) >= int(self.settings.get("min_height")):  # and image["height"] <= image["width"]):
-                images.append(index)
+                    if int(image["width"]) >= int(self.settings.get("min_width")) and int(image["height"]) >= int(self.settings.get("min_height")):  # and image["height"] <= image["width"]):
+                        images.append(index)
+                except KeyError:
+                    print("Entry with no preview")
+        except TypeError:
+            GLib.idle_add(self.set_progress, 1, "Error scanning for images.")
+            time.sleep(5)
+            GLib.idle_add(self.finish_load)
+            return
+            
 
         GLib.idle_add(self.set_progress, 0.5, "Downloading thumbnails...")
         length = len(images)
@@ -196,14 +288,16 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
         for num in images:
             GLib.idle_add(self.set_progress, curr_len, "Downloading thumbnails... (" + str(int(((curr_len - 0.5) / 0.5) * length) + 1) + " of " + str(length) + ")")
             with open(self.storage_directory + "/Thumbnails/thumb_" + str(num), 'wb') as f:
-                f.write(requests.get(self.reddit["data"]["children"][num]["data"]["thumbnail"]).content)
+                try:
+                    f.write(requests.get(self.reddit["data"]["children"][num]["data"]["thumbnail"]).content)
+                except requests.exceptions.MissingSchema:
+                    print("Failed to fetch thumbnail from " + self.reddit["data"]["children"][num]["data"]["thumbnail"])
         
             GLib.idle_add(self.preview_new, num)
             curr_len += 0.5/length
         GLib.idle_add(self.finish_load)
 
     def preview_new(self, num):
-
         button = Gtk.Button()
 
         image = Gtk.Image().new_from_file(self.storage_directory + "/Thumbnails/thumb_" + str(num))
@@ -238,19 +332,16 @@ class RedditWallpaperWindow(Gtk.Window):  # Main window
             print("Couldn't edit exif data")
 
         GLib.idle_add(self.set_progress, 0.8, "Setting wallpaper")
+        self.run_wallpaper_manager(title)
+        time.sleep(0.25)
+        GLib.idle_add(self.finish_load)
+        self.refreshRecents()
+
+    def run_wallpaper_manager(self, title):
         if self.settings.get("wallpaper_manager") == "GNOME":
             subprocess.run(['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', 'file://' + self.storage_directory + "/Wallpapers/" + title])
         elif self.settings.get("wallpaper_manager") == "KDE":
-            subprocess.run(['dbus-send', '--session', '--dest=org.kde.plasmashell', '--type=method_call', '/PlasmaShell', 'org.kde.PlasmaShell.evaluateScript',"string: \n\
-var Desktops = desktops(); \n\
-for (i=0;i<Desktops.length;i++) { \n\
-        d = Desktops[i]; \n\
-        d.wallpaperPlugin = \"org.kde.image\"; \n\
-        d.currentConfigGroup = Array(\"Wallpaper\", \n\
-                                    \"org.kde.image\", \n\
-                                    \"General\"); \n\
-        d.writeConfig(\"Image\", \"file://" + self.storage_directory + "/Wallpapers/" + title + "\"); \n\
-}"])
+            subprocess.run(['dbus-send', '--session', '--dest=org.kde.plasmashell', '--type=method_call', '/PlasmaShell', 'org.kde.PlasmaShell.evaluateScript',"string: \nvar Desktops = desktops(); \nfor (i=0;i<Desktops.length;i++) { \n        d = Desktops[i]; \n        d.wallpaperPlugin = \"org.kde.image\"; \n        d.currentConfigGroup = Array(\"Wallpaper\", \n                                    \"org.kde.image\", \n                                    \"General\"); \n        d.writeConfig(\"Image\", \"file://" + self.storage_directory + "/Wallpapers/" + title + "\"); \n}"])
             subprocess.run(['kwriteconfig5', '--file', 'kscreenlockerrc', '--group', 'Greeter', '--group', 'Wallpaper', '--group', 'org.kde.image', '--group', 'General', '--key', 'Image', 'file://' + self.storage_directory + "/Wallpapers/" + title])
         elif self.settings.get("wallpaper_manager") == "feh":
             subprocess.run(['feh', '--bg-fill', self.storage_directory + "/Wallpapers/" + title])
@@ -260,9 +351,6 @@ for (i=0;i<Desktops.length;i++) { \n\
             command = self.settings.get("wallpaper_manager")
             command = command.replace("$wallpaper", self.storage_directory + "/Wallpapers/" + title)
             subprocess.run(["bash", "-c", command])
-
-        time.sleep(0.25)
-        GLib.idle_add(self.finish_load)
 
     def setup_settings(self):
         label = Gtk.Label()
@@ -383,8 +471,17 @@ for (i=0;i<Desktops.length;i++) { \n\
     def on_settings_click(self, sender):
         if sender.get_active():
             self.right.show_all()
+            self.hbsort.hide()
+            self.settings.set("show_settings_pane", True)
         else:
             self.right.hide()
+            self.hbsort.show_all()
+            self.settings.set("show_settings_pane", False)
+
+        for index, i in enumerate(self.settings.sort_types):
+            if i == self.settings.get("sort"):
+                self.hbsort.set_active(index)
+                self.sort.set_active(index)
 
     def on_close(self, args):
         with os.scandir(self.storage_directory + "/Thumbnails") as it:
@@ -399,7 +496,6 @@ for (i=0;i<Desktops.length;i++) { \n\
 
 
 class Settings:  # Settings object
-
     # Global variables used:
     #   default_json (string): Stores the default settings, used for creating new config.json, verifying the keys of config.json, and resetting config.json back to default.
     #   location (string): Storage location, typically ~/.local/share/reddit_wallpaper
@@ -407,7 +503,7 @@ class Settings:  # Settings object
     #   settings (dictionary): Array containing all keys and values stored in settings
     #   default_settings (JSON Object): Default settings loaded from default_json string
 
-    default_json = '{"subreddits": "EarthPorn+BotanicalPorn+WaterPorn+SeaPorn+SkyPorn+DesertPorn+LakePorn", "sort": "top/?t=week", "wallpaper_manager": "GNOME", "min_width": "1920", "min_height": "1080"}'
+    default_json = '{"subreddits": "EarthPorn+BotanicalPorn+WaterPorn+SeaPorn+SkyPorn+DesertPorn+LakePorn", "sort": "top/?t=week", "wallpaper_manager": "GNOME", "min_width": "1920", "min_height": "1080", "show_settings_pane": true}'
     settings = {}
     sort_types = ("hot", "new", "top/?t=hour", "top/?t=day", "top/?t=week", "top/?t=month", "top/?t=year", "top/?t=all", "random")
 
@@ -470,6 +566,17 @@ class Settings:  # Settings object
             except:
                 self.throw_error("Invalid value for key \"min_height\". Value must be an integer above 0.")
                 return False
+        elif key == "show_settings_pane":
+            try:
+                if isinstance(value, bool):
+                    settings[key] = value
+                else:
+                    self.throw_error("Invalid value for key \"show_settings_pane\". Value must be boolean.")
+                    return False
+            except:
+                self.throw_error("Invalid value for key \"show_settings_pane\". Value must be boolean.")
+                return False
+
         else:
             self.throw_error("Error: Invalid key.")
             return False
